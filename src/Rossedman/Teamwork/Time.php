6bb2f5fbd9d547301ac59beff451966104aff92c
Mateusz Grochala <mateusz.grochala@yahoo.dk>
2017-09-13 15:12:08 +0200

    Added methods for time-entries
    find() - single time entry
    edit()
    delete()

--- src/Rossedman/Teamwork/Time.php
+++ src/Rossedman/Teamwork/Time.php
@@ -21,4 +21,39 @@ class Time extends AbstractObject {
 
         return $this->client->get($this->endpoint, $args)->response();
     }
+
+    /**
+     * GET /time_entries/id.json
+     *
+     * @return mixed
+     */
+    public function find($args = null)
+    {
+        $this->areArgumentsValid($args, ['page']);
+
+        return $this->client->get("$this->endpoint/$this->id.json", $args)->response();
+    }
+
+    /**
+     * Edit A Time entry
+     * PUT /time_entries/{id}.json
+     *
+     * @return mixed
+     */
+    public function edit($data)
+    {
+        return $this->client->put("$this->endpoint/$this->id.json", ['time-entry' => $data])->response();
+    }
+
+    /**
+     * Delete A Time entry
+     * DELETE /time_entries/{id}.json
+     *
+     * @return mixed
+     */
+    public function delete()
+    {
+        return $this->client->delete("$this->endpoint/$this->id.json")->response();
+    }
+
 }
